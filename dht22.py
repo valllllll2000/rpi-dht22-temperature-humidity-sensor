89d8511f3bcd00f00bd3ea58b6e77c6386887bee
Valeria R <valerie.cardoso@gmail.com>
2024-01-17 21:31:39 +0100

    Update dht22.py

--- dht22.py
+++ dht22.py
@@ -1,37 +1,38 @@
-""" 
-dht22.py 
-Temperature/Humidity monitor using Raspberry Pi and DHT22. 
-Data is displayed at thingspeak.com
-Original author: Mahesh Venkitachalam at electronut.in 
-Modified by Adam Garbo on December 1, 2016 
-""" 
-import sys 
-import RPi.GPIO as GPIO 
-from time import sleep 
-import Adafruit_DHT 
-import urllib2 
-myAPI = "THINGSPEAK WRITE API_KEY" 
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
 
-def getSensorData(): 
-   RH, T = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4) 
-   return (str(round(RH, 1)), str(round(T,1))) 
+import time
+import board
+import adafruit_dht
 
-def main(): 
-   print 'starting...' 
-   baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI 
-   while True: 
-       try: 
-           RH, T = getSensorData() 
-	   print RH, T
-           f = urllib2.urlopen(baseURL + 
-                               "&field1=%s&field2=%s" % (RH, T)) 
-           print f.read() 
-           f.close() 
-           sleep(300) #uploads DHT22 sensor values every 5 minutes 
-       except: 
-           print 'exiting with error' 
-           break 
+myAPI = "THINGSPEAK WRITE API_KEY"
 
-# call main 
-if __name__ == '__main__': 
-   main()
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT22(board.D4)
+
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+
+while True:
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} C    Humidity: {}% ".format(
+                temperature_c, humidity
+            )
+        )
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(2.0)
